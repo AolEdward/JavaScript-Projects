4278f4548195129ec3c770c659b4090bba87621a
AolEdward <edwardcaluag20@gmail.com>
2023-09-15 01:01:44 -0600

    Update main.js
    added comments

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,11 +1,24 @@
 document.write("Hello, This is an external Javascript  ");
+document.write("<br>");
+
 
 var A = "This is a string";
 
 document.write(A);
 
+
 var B = "Click Button to Close Message.";
+document.write("<br>");
 
 window.alert
 
 document.write(A, " Concatenating Var A ");
+document.write("<br>"); //I tried to do the next line
+
+var Family = "The Smiths", Dad = "John", Mom = "Mary", Daughter ="Jane", Son = "Peter";
+
+document.write(Mom);
+
+document.write("<br>");
+
+document.write(3+3); //Expressions
