b695b5ae423d2e856a59c5722c5193998c8d30c8
AolEdward <edwardcaluag20@gmail.com>
2023-09-15 01:15:42 -0600

    Update main.js
    Basic Javascript Project Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,18 +1,20 @@
+
+var B = "Click Button to Close Message.";
+window.alert(B)
+
 document.write("Hello, This is an external Javascript  ");
 document.write("<br>");
 
 
-var A = "This is a string";
+var A = "This is the beginning of the string";
 
 document.write(A);
-
-
-var B = "Click Button to Close Message.";
 document.write("<br>");
 
-window.alert
+var C =" and this is the end of the string";
+var D = (A+C);
 
-document.write(A, " Concatenating Var A ");
+document.write(D)
 document.write("<br>"); //I tried to do the next line
 
 var Family = "The Smiths", Dad = "John", Mom = "Mary", Daughter ="Jane", Son = "Peter";
