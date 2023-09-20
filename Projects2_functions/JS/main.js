e51efa626a90ed1109fc49535e846a5e480be4fd
AolEdward <edwardcaluag20@gmail.com>
2023-09-20 00:47:10 -0600

    update
    update

---
+++ Projects2_functions/JS/main.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var sentence = " I am learning";
+    sentence += " a lot from this boook";
+    document.getElementById("demo").innerHTML = sentence;
+    
+}
+
+
+function myTime() {
+    var time = Date();
+    document.getElementById("clock").innerHTML = time;
+}
