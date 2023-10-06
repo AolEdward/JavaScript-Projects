e2fe7f351a2729c81ed5d6457021200b9c098b37
AolEdward <edwardcaluag20@gmail.com>
2023-10-06 01:06:54 -0600

    Operations
    Addition,Subtraction,Multiplication and Division

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,20 @@
+function addition_function() {
+    var addtion = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addtion
+}
+
+function subtraction_function() {
+    var subtraction = 5 - 2;
+    document.getElementById("Math2").innerHTML = "5 - 2 = " + subtraction;
+
+}
+
+function multiplication(){
+    var simple_math = 6 * 8;
+    document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_math;
+}
+
+function division(){
+    var simple_math2 = 48 / 6;
+    document.getElementById("Math4").innerHTML = "48 ÷ 6 = " + simple_math2;
+}
--- Projects2_functions/JS/main.js
+++ Projects2_functions/JS/main.js
@@ -10,3 +10,9 @@ function myTime() {
     var time = Date();
     document.getElementById("clock").innerHTML = time;
 }
+
+function myFunction2() {
+    var addition =  2 + 2;
+    document.getElementById("demo2").innerHTML = "2 + 2 = " + addition;
+
+}
