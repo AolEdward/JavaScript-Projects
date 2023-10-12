4effb87dccc2886094411db4b41fe3f8429a4aef
AolEdward <edwardcaluag20@gmail.com>
2023-10-11 19:50:36 -0600

    Updated
    Updated with comments

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,24 +1,24 @@
-window.alert("Here is a random number " +  Math.random() * 100);
+window.alert("Here is a random number " +  Math.random() * 100); //Random Picker
 
 function addition_function() {
     var addtion = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addtion
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addtion; //Using addition
 }
 
 function subtraction_function() {
     var subtraction = 5 - 2;
-    document.getElementById("Math2").innerHTML = "5 - 2 = " + subtraction;
+    document.getElementById("Math2").innerHTML = "5 - 2 = " + subtraction;  //Using Subtraction
 
 }
 
 function multiplication(){
     var simple_math = 6 * 8;
-    document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_math;
+    document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_math; //Multiplication
 }
 
 function division(){
     var simple_math2 = 48 / 6;
-    document.getElementById("Math4").innerHTML = "48 ÷ 6 = " + simple_math2;
+    document.getElementById("Math4").innerHTML = "48 ÷ 6 = " + simple_math2; //Division
 }
 
 function modulus_operator(){
@@ -28,7 +28,7 @@ function modulus_operator(){
 
 function negation_operator(){
     var x =10;
-    document.getElementById("Math6").innerHTML = -x;
+    document.getElementById("Math6").innerHTML = -x; //Negation
 }
 
 var X = 5;
