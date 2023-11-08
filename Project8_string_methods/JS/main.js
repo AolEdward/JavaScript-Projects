f70d13f63f38d3df7bd228d7b17d6448bda5978a
AolEdward <edwardcaluag20@gmail.com>
2023-11-07 23:00:40 -0700

    Project 8
    project 8

---
+++ Project8_string_methods/JS/main.js
@@ -0,0 +1,15 @@
+function full_sentence(){
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence.";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+
+}
+
+function slice_method(){
+    var sentence = "All work and no play makes Johnny a dull boy.";
+    var section = sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = section;
+}
